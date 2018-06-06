97de799c2cbac31ba3a44c9b7c9b075968893bae
unknown <bjoren92@gmail.com>
2018-06-06 23:39:25 +0200

    Right click now adds a user input marker

--- Web-server/script.js
+++ Web-server/script.js
@@ -4,7 +4,7 @@
 
 this.getConfig = function(successCallback, errorCallback) {
 	$.ajax ({
-	        url: 'http://localhost:3000/config',
+	        url: 'http://' + window.location.hostname + ':3000/config', //TODO: Remove hardcoding of port
 	        datatype: "json",
 	        success: successCallback,
 	        error: errorCallback
@@ -13,7 +13,7 @@ this.getConfig = function(successCallback, errorCallback) {
 
 this.getReports = function(successCallback, errorCallback) {
 	$.ajax ({
-	        url: 'http://localhost:3000/reports',
+	        url: 'http://' + window.location.hostname + ':3000/reports', //TODO: Remove hardcoding of port
 	        datatype: "json",
 	        success: successCallback,
 	        error: errorCallback
@@ -22,19 +22,37 @@ this.getReports = function(successCallback, errorCallback) {
 
 //Map methods -----------------------------------------------
 
-this.setUpMap = function(CONFIG) {
-        map = L.map('mapid').setView([CONFIG.startingPointX, CONFIG.startingPointY], CONFIG.startingPointZoomLevel);
+this.onRightClick = function(event){
+	if(userReportMarker != null) { map.removeLayer(userReportMarker); } //Remove old marker
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox.streets',
-            accessToken: CONFIG.accessToken
-        }).addTo(map);
+	console.log(event);
+	
+	userReportMarker = L.marker(event.latlng, {"draggable": true}).bindPopup("event.latlng").openPopup();
+
+	userReportMarker.addTo(map).openPopup();
+
+	//Pop up data form
+
+	//Fire post report to DB
+
+	//Update reportsList and run setUpReports
+}
+
+this.setUpMap = function(CONFIG) {
+    map = L.map('mapid').setView([CONFIG.startingPointX, CONFIG.startingPointY], CONFIG.startingPointZoomLevel);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: CONFIG.accessToken
+    }).addTo(map);
+
+    map.on('contextmenu', onRightClick);
 }
 
 this.createPopupText = function(report) {
-	return "<b>Quest: </b>" + report.quest + 
+	return "<b>Task: </b>" + report.quest + 
 		"<br><b>Reward: </b>" + report.reward;
 }
 
@@ -44,13 +62,15 @@ this.createIcon = function(reward) {
 	if(iconUrl != null) {
 		var rewardIcon = L.icon({
 		    iconUrl: iconUrl,
-		    iconSize: [40, 40]
+		    iconSize: [40, 40],
+		    popupAnchor: [-0, -15]
 		});
 		return {icon: rewardIcon};
 	}
 }
 
 this.setUpReports = function(reports) {
+
 	reportsList = reports;
 
 	for (var i = reports.length - 1; i >= 0; i--) {
@@ -63,6 +83,7 @@ this.setUpReports = function(reports) {
 
 //Main flow---------------------------------------------------
 
+var userReportMarker;
 var reportsList;
 var map;
 
