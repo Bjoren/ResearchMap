360f99535e103f7570b227624abbe21b7484bea4
Bjoren <Bjoren92@gmail.com>
2019-05-20 22:53:50 +0200

    change web server to use lat/lng instead of x/y from config

--- Web-server/script.js
+++ Web-server/script.js
@@ -29,7 +29,7 @@ this.submitReport = function() {
 //Map methods -----------------------------------------------
 
 this.setUpMap = function(CONFIG) {
-    map = L.map('mapid').setView([CONFIG.startingPointX, CONFIG.startingPointY], CONFIG.startingPointZoomLevel);
+    map = L.map('mapid').setView([CONFIG.startingPointLat, CONFIG.startingPointLng], CONFIG.startingPointZoomLevel);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
