a8258720250e910c6521befd4f74e8435821eb74
unknown <bjoren92@gmail.com>
2019-05-24 22:58:48 +0200

    Improve error handling

--- API-server/api/routes/apiRoutes.js
+++ API-server/api/routes/apiRoutes.js
@@ -6,15 +6,23 @@ module.exports = function(app) {
         reportController = require('../controllers/reportController'),
         configController = require('../controllers/configController');
 
+    //app.set("env", "production"); //TODO: This will remove stacktraces from API-response
+    
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
 
-    app.use(function(err, req, res, next) {
+    app.use(function(err, req, res, next) { //Catch errors to override default error handler which sends HTML instead of JSON
         console.log("Test: " + JSON.stringify(err));
-        next();
+        if(err.type === "entity.parse.failed") {
+            res.status(400);
+            res.json({"error": (err.message)});
+        } else {
+            res.status(500);
+            res.json({"error": (err.message)}); //TODO: Investigate if it's a bad idea to expose this via API.
+        }
     })
 
     app.route('/pokestops')
